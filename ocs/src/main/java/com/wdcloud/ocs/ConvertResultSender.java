423fe622aaba77f5827d722fd4602c34c712b993
zhaoxiufei@gmail.com <zhaoxiufei@gmail.com>
2018-11-20 16:59:41 +0800

    update

--- ocs/src/main/java/com/wdcloud/ocs/ConvertResultSender.java
+++ ocs/src/main/java/com/wdcloud/ocs/ConvertResultSender.java
@@ -5,7 +5,6 @@ import com.wdcloud.mq.model.MqConstants;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.core.AmqpTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.stereotype.Service;
 
 @Slf4j
@@ -15,17 +14,7 @@ public class ConvertResultSender {
     @Autowired
     private AmqpTemplate amqpTemplate;
 
-    @Autowired
-    private ThreadPoolTaskExecutor threadPoolTaskExecutor;
-
     public void send(ConvertResultMQO mqo) {
-        threadPoolTaskExecutor.execute(() -> {
-            try {
-                Thread.sleep(500);//TODO 转码任务 //TODO 异常扩展
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-            amqpTemplate.convertAndSend(MqConstants.TOPIC_EXCHANGE_LMS, MqConstants.QUEUE_LMS_CONVERT_RESULT, mqo);
-        });
+        amqpTemplate.convertAndSend(MqConstants.TOPIC_EXCHANGE_LMS, MqConstants.QUEUE_LMS_CONVERT_RESULT, mqo);
     }
 }
