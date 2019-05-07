0032eb487106e5a3db1e7ba51893d1f77331ee2d
zhaoxiufei@gmail.com <zhaoxiufei@gmail.com>
2019-05-07 14:58:56 +0800

    fixed

--- oss/src/main/java/com/wdcloud/oss/mq/MqConfig.java
+++ oss/src/main/java/com/wdcloud/oss/mq/MqConfig.java
@@ -10,7 +10,6 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 
-@ComponentScan
 @Configuration
 public class MqConfig {
 
